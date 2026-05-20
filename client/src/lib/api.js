8bbcbab123afbd53e1ae5881f265079d77d7eb79
TF <83311440+tiagofachini@users.noreply.github.com>
2026-05-20 09:43:25 -0300

    fix: adiciona apikey ao publicApi para autorizar chamadas públicas ao Supabase

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -13,6 +13,9 @@ api.interceptors.request.use(async (config) => {
 
 export default api
 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZ2V4bHNmbXlmeXB1ZXNsenhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NTM4OTUsImV4cCI6MjA4MjMyOTg5NX0.mqKyQdH9kl4msSONSjsqp0n3Z8HxHskwrK9WtqwJfo0'
+
 export const publicApi = axios.create({
   baseURL: 'https://nfgexlsfmyfypueslzxo.supabase.co/functions/v1',
+  headers: { apikey: SUPABASE_ANON_KEY },
 })
